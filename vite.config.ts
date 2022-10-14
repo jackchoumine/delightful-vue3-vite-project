502bbf835e37f9dcf269d05edcea39a2981fd1c5
zhouqijun <jackchou4job@163.com>
2022-10-14 18:07:48 +0800

    chore: 配置自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,27 @@
+/*
+ * @Date        : 2022-08-08 15:54:09
+ * @Author      : ZhouQijun
+ * @LastEditors : ZhouQijun
+ * @LastEditTime: 2022-10-14 16:29:52
+ * @Description : vite 配置
+ */
 // @ts-nocheck
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue'] //, 'vue-router']
+    })
+  ],
+  server: {
+    open: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
