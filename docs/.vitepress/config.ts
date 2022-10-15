aff2fadb1504e3a9dff74fc1608a009db901830b
jack <jackzhoumine@gmail.com>
2022-10-16 03:54:01 +0800

    feat: 添加文档

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,117 @@
+/*
+ * @Description : vitepress 配置
+ * @Date        : 2022-10-16 02:33:56 +0800
+ * @Author      : JackChou
+ * @LastEditTime: 2022-10-16 03:53:40 +0800
+ * @LastEditors : JackChou
+ */
+export default {
+  title: 'Jack 的博客',
+  description: 'vue 教程', // html meta description 方便搜索引擎抓取
+  head: [['link', { rel: 'icon', href: '/logo.gif' }]],
+  markdown: {
+    lineNumbers: true, // 代码块显示行号
+    toc: {
+      includeLevel: [1, 2, 3, 4] //生成目录的 markdown 标题 默认 2 3
+    }
+  },
+  themeConfig: {
+    // setTitle: 'hello',
+    logo: '/favicon.ico',
+    sidebar: 'auto', // 所有页面开启自动根据 markdown 的标题生成侧边栏
+    lastUpdated: true, // 根据 git commit 的时间显示最后更新时间
+    nav: [
+      { text: '主页', link: '/' },
+      // {
+      //   text: '框架',
+      //   // 二级下拉
+      //   items: [
+      { text: 'vue2', link: '/vue2/' },
+      { text: 'vue3', link: '/vue3/' },
+      { text: 'react', link: '/react/' },
+      //   ]
+      // },
+      { text: 'node', link: '/node/' },
+      { text: '科学', link: '/science/' },
+      {
+        text: 'web',
+        items: [
+          {
+            items: [
+              { text: 'js', link: '/web/js/' },
+              { text: 'css', link: '/web/css/' }
+              // { text: 'html', link: '/web/html/' }
+            ]
+          },
+          { items: [{ text: 'webpack', link: '/webpack/' }] }
+        ]
+      },
+      {
+        text: '其他',
+        // 二级下拉
+        items: [
+          { text: '函数式编程', link: '/others/functional-programming/' },
+          { text: 'angular', link: '/angular/' },
+          { text: '开发工具', link: '/others/dev-tool/' },
+          { text: 'mac使用', link: '/others/mac/' }
+        ]
+      }
+      // { text: '面试', link: '/interview/' },
+    ],
+    // 博客仓库配置
+    repo: 'https://github.com/jackchoumine/vue-press-blog',
+    repoLabel: 'Github',
+    // 开启页面上的编辑此页
+    editLinks: true,
+    editLinkText: '编辑此页',
+    // algolia 搜索全文，需要 apiKey
+    // 内置搜索智能搜索 h2 h3 标题
+    algolia: {
+      apiKey: 'b7968a5518faded4fab25c0e7e60813a',
+      indexName: 'jackBlog'
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://gitee.com/geeksdidi' }
+      // { icon: 'twitter', link: '...' },
+      // // You can also add custom icons by passing SVG as string:
+      // {
+      //   icon: {
+      //     svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>'
+      //   },
+      //   link: '...'
+      // }
+    ]
+  },
+  plugins: [
+    ['@mr-hope/copy-code'],
+    [
+      'vuepress-plugin-mygitalk',
+      {
+        // 是否启用(关闭请设置为false)(default: true)
+        enable: true,
+        // 是否开启首页评论(default: true)
+        home: true,
+        // Gitalk配置
+        gitalk: {
+          // GitHub Application Client ID.
+          clientID: '8dc7d58ba7eb7f5f8754',
+          // GitHub Application Client Secret.
+          clientSecret: '71ecf094b54febb001f2b08723a023b7f26e9d69',
+          // GitHub repository. 存储评论的 repo
+          repo: 'vue-press-blog',
+          // GitHub repository 所有者，可以是个人或者组织。
+          owner: 'jackchoumine',
+          // 设置语言(default: zh-CN)
+          language: 'zh-CN'
+        }
+      }
+    ]
+  ],
+  // base: '/blog/',
+  // 构建输出目录
+  dest: './dist', // 默认 .vuepress/dist
+  // 开发运行的端口
+  port: '7777',
+  // 开发运行的地址
+  host: '127.0.0.1'
+}
---
+++ docs/index.md
@@ -0,0 +1,70 @@
+---
+home: true
+lang: zh-CN
+heroImage: https://tva1.sinaimg.cn/large/008i3skNgy1gr80pnhlmcj3050050mxi.jpg
+heroText: JackChouMine
+tagline: Jack 个人博客
+actionText: 开始阅读 →
+actionLink: /vue2/
+---
+
+<!-- features:
+  - title: Wiki - 求索
+    details: 基于书签对知识点进行 整理，吸收，吐纳，归档。吾将上下而求索...
+  - title: Store - 仓库
+    details: 展望云仓库而归纳整理，方便行事&培养习惯。鱼和熊掌我全都要...
+  - title: Thought - 随笔
+    details: 记录一些人生感悟、想法...
+footer: MIT Licensed | Copyright © 2019.01.11-present JackChou -->
+
+## 最近的文章
+
+### [vue 指令总结](./vue3/指令.md)
+
+### [如何检测一个元素是否在视窗中](./vue3/如何检测一个元素是否在视窗中.md)
+
+### [vue 代码优化方案](./vue3/vue代码优化方案.md)
+
+### [组件设计之分离公共行为](./vue3/组件设计之分离公共行为.md)
+
+### [组件设计之 renderProp](./vue3/组件设计之render-prop.md)
+
+### [组件设计之受控组件](./vue3/组件设计之受控组件.md)
+
+### [vue2 响应式系统核心原理实现](./vue2/vue2响应式系统核心原理实现.md)
+
+### [中医的玄学之路](./science/中医的玄学之路.md)
+
+### [中医与巫术](./science/中医与巫术.md)
+
+### [rollup 基础使用](./webpack/rollup基础使用.md)
+
+### [webpack5 模块联邦实现微前端](./webpack/模块联邦实现微前端.md)
+
+### [stencil 组件](./web/js/stencil组件.md)
+
+### [stencil 简介](./web/js/stencil简介.md)
+
+### [vue3 的 render 函数有哪些变化?](./vue3/render函数.md)
+
+### [如何更好的管理公共组件?](./vue2/如何更好的管理公共组件.md)
+
+### [如何设计一个组件？常见的 React 组件设计模式 --- renderProp](./react/render-props.md)
+
+### [如何设计一个组件？常见的 React 组件设计模式 --- 复合组件](./react/compound-component.md)
+
+### [如何设计一个组件？常见的 React 组件设计模式 --- 受控的复合组件](./react/control-props.md)
+
+### [如何优雅地在本地测试 npm 包？](./node/如何在本地测试npm包.md)
+
+### [箭头函数和普通函数的有哪些区别？](./web/js/箭头函数和普通函数的区别.md)
+
+### [如何优雅地封装 axios?](./web/js/封装axios.md)
+
+### [如何使用 render 函数封装高扩展的组件?](./vue2/如何使用render函数封装高扩展的组件.md)
+
+### [react 核心概念](./react/react-basic.md)
+
+### [函数式编程概述](./others/functional-programming/函数式编程概述.md)
+
+### [函数计算得到函数](./others/functional-programming/函数计算得到函数.md)
