6f93f40f07f1bffe87d258b30d178db4ce6d7b0a
zhouqijun <jackchou4job@163.com>
2022-10-17 09:35:56 +0800

    chore: 添加文件

--- src/components/Index.ts
+++ src/components/Index.ts
@@ -2,8 +2,10 @@
  * @Description : 导出组件
  * @Date        : 2022-10-15 19:51:59 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-10-15 19:53:15 +0800
- * @LastEditors : JackChou
+ * @LastEditTime: 2022-10-17 09:35:23
+ * @LastEditors : ZhouQijun
  */
 export { default as HelloWorld } from './HelloWorld.vue'
 export { FileViewer } from './FileViewer'
+
+//test
