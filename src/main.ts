fed3d2a885fbf4d5f490bb98bf0880cc1bae0536
zhouqijun <jackchou4job@163.com>
2022-10-18 15:54:52 +0800

    chore: 使用web-component

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,20 @@
  * @Date        : 2022-08-08 14:23:25
  * @Author      : ZhouQijun
  * @LastEditors : ZhouQijun
- * @LastEditTime: 2022-10-18 13:01:58
+ * @LastEditTime: 2022-10-18 15:48:10
  * @Description :
  */
 import { createApp } from 'vue'
-// import './style.css'
 import App from './App.vue'
 import { vAuth, vClickOutside } from './plugins'
 
 // import { defineCustomElements } from 'jack-ui/loader'
-import { CountTo } from 'jack-ui'
 // defineCustomElements()
+
+// import './assets/styles/style.css'
+
+import 'jack-web-ui/dist/jack-web-ui/jack-web-ui.css'
+import { CountTo } from 'jack-web-ui'
 customElements.define('count-to', CountTo as unknown as CustomElementConstructor)
 
 const app = createApp(App)
