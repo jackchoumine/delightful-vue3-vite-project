439b68634cef2f202e2a89dd04857ac504063f9e
zhouqijun <jackchou4job@163.com>
2022-11-12 16:54:58 +0800

    wip: 封装播放器

--- src/components/HookTest/HookDemo.vue
+++ src/components/HookTest/HookDemo.vue
@@ -2,18 +2,20 @@
  * @Date        : 2022-11-10 11:35:34
  * @Author      : ZhouQiJun
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2022-11-10 11:35:37
+ * @LastEditTime: 2022-11-12 15:28:34
  * @Description : 
 -->
 <template>
   <div>
     <h2>测试hook</h2>
     <TestHook />
+    <VideoPlayer />
   </div>
 </template>
 
 <script setup lang="ts">
   import TestHook from './HookTest.vue'
+  import VideoPlayer from '../VideoPlayer/VideoPlayer.vue'
 </script>
 
 <style lang="scss"></style>
