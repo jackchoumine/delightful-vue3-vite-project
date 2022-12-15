84ca558b007c01afb19958205cdd1f903c58be9b
ZhouQiJun <jackchou4job@163.com>
2022-12-15 17:36:09 +0800

    feat: useEvent

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,9 @@
+import { type } from 'os'
+
 export { copyToClipboard, copyText, addWaterMarker } from './tools'
 export type { WaterMakerParams } from './tools'
+export { default as EventBus } from './EventBus'
+export type { Cb, Off } from './EventBus'
 
 export function add(a, b) {
   return a + b
